4bdb857afa240f7c03ef7e9edc37fc86968d3bb7
LamNX <lamnx@miichisoft.net>
2023-11-09 14:03:55 +0700

    add header to layouts

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,13 @@
 import { AppBar, Box, IconButton, Stack, styled, Toolbar, Typography } from "@mui/material"
-import { useAppSelector } from "../../redux/Store"
+import { useAppDispatch, useAppSelector } from "../../redux/Store"
 import { Login, Logout } from "@mui/icons-material"
+import { login, logout } from "../../redux/AuthSlice"
 
 
 export const Header = () => {
   const configState = useAppSelector((state) => state.config)
   const authState = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch()
 
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
     boxShadow: 'none',
@@ -20,12 +22,18 @@ export const Header = () => {
     width: '100%',
     color: theme.palette.text.secondary,
   }))
+
+  const toggleLogin = () => {
+    dispatch(authState.token ? logout() : login("test", 'test'))
+  }
+  console.log(authState)
+
   return <AppBarStyled position={"sticky"} color={"secondary"}>
     <ToolbarStyled>
       <Typography>LOGO HERE</Typography>
       <Box flexGrow={1}/>
       <Stack spacing={1} direction="row" alignItems="center">
-        <IconButton>
+        <IconButton onClick={toggleLogin}>
           {authState.token ? <Logout/> : <Login/>}
         </IconButton>
       </Stack>
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,11 +3,13 @@ import { Loadable } from "../components/common/Loadable"
 import { useRoutes } from "react-router-dom"
 import { ProtectedRouter } from "./ProtectedRouter"
 import { PublicRouter } from "./PublicRouter"
+import { useAppSelector } from "../redux/Store"
 
 const Home = Loadable(lazy(() => import('../pages/Home')))
 
 export const Router = () => {
-  const [isAuth, setIsAuth] = useState(false)
+  const authState = useAppSelector(state => state.auth)
+  const isAuth = !!authState.token
   const routes = useRoutes(isAuth ? ProtectedRouter : PublicRouter)
   return routes
 }
